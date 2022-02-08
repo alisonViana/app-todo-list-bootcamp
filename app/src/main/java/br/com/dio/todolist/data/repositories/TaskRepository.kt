7a7d177126dbc66ddd2716996f36c1cbd5232b5b
alisonViana <alisonviana.c@gmail.com>
2022-02-08 02:29:29 -0300

    removes non use lines

--- app/src/main/java/br/com/dio/todolist/data/repositories/TaskRepository.kt
+++ app/src/main/java/br/com/dio/todolist/data/repositories/TaskRepository.kt
@@ -2,9 +2,6 @@ package br.com.dio.todolist.data.repositories
 
 import br.com.dio.todolist.data.model.Task
 import br.com.dio.todolist.data.services.TaskDao
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class TaskRepository(private val dao: TaskDao) {
 
@@ -21,36 +18,4 @@ class TaskRepository(private val dao: TaskDao) {
     suspend fun update(task: Task) {
         dao.update(task)
     }
-
-    /*
-    fun getAll() = runBlocking {
-        dao.getAll()
-    }
-
-
-    fun insert(task: Task) = runBlocking {
-        launch(Dispatchers.IO) {
-            try {
-                dao.insert(task)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            }
-        }
-    }
-
-    fun delete(task: Task) = runBlocking {
-        launch(Dispatchers.IO) {
-            try {
-                dao.delete(task)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            }
-        }
-    }
-
-    fun update(task: Task) = runBlocking {
-        launch(Dispatchers.IO) {  }
-    }
-     */
-
 }
